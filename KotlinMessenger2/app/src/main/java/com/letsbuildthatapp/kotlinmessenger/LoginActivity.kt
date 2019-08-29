2aff6234d4fc05eef58162bda2f2ebd92e7229c1
Vasiliy <smirnov.outrun@gmail.com>
2019-08-29 14:14:59 +0300

    Create and connect Firebase for app I saw new methods Was changed fields and buttons for rounded style Turned interection for two screens and turned correctness of input info

--- KotlinMessenger2/app/src/main/java/com/letsbuildthatapp/kotlinmessenger/LoginActivity.kt
+++ KotlinMessenger2/app/src/main/java/com/letsbuildthatapp/kotlinmessenger/LoginActivity.kt
@@ -2,7 +2,9 @@ package com.letsbuildthatapp.kotlinmessenger
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity:AppCompatActivity() {
@@ -17,7 +19,13 @@ class LoginActivity:AppCompatActivity() {
             val email = email_edittext_login.text.toString()
             val password = password_edittext_login.text.toString()
 
+
+
             Log.d("Login","Attempt with email/pw: $email/***")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            // .addOnCompleteListener()
+            // .add
 
 
         }
